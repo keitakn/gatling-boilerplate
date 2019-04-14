9f768eef908c15ce2aa6c1203f1655c66f4eea55
keitakn <keita.koga.work@gmail.com>
2019-04-14 23:48:40 +0900

    :recycle: #3 Qiitaのアクセストークンを外部から渡せるように改修

--- src/test/scala/qiita/FetchUsersItemsSimulation.scala
+++ src/test/scala/qiita/FetchUsersItemsSimulation.scala
@@ -1,14 +1,24 @@
 package qiita
 
+import scala.concurrent.duration._
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 class FetchUsersItemsSimulation extends Simulation {
   val userFeeder = csv("qiita/users.csv").circular
 
+  val accessToken = sys.env.getOrElse("QIITA_ACCESS_TOKEN", "")
+  val sentHeaders = Map("Authorization" -> "Bearer ".+(accessToken))
+
   val httpProtocol = http
     .baseUrl("https://qiita.com")
     .acceptHeader("application/json")
+
+  if (accessToken != "") {
+    println(accessToken)
+    http.headers(sentHeaders)
+  }
 
   val scn = scenario("Fetch Qiita Users")
     .feed(userFeeder)
@@ -31,5 +41,11 @@ class FetchUsersItemsSimulation extends Simulation {
         .check(status.is(200))
     )
 
-  setUp(scn.inject(atOnceUsers(3)).protocols(httpProtocol))
+  setUp(
+    scn
+      .inject(
+        constantUsersPerSec(3) during (1 second)
+      )
+      .protocols(httpProtocol)
+  )
 }
